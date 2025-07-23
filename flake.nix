55b0f589e33a5e86e2615a6772e25f9b1134cf42
sh4k0 <carlofedep@gmail.com>
2025-07-23 16:13:53 +0200

    flake.nix: added python libraries

--- flake.nix
+++ flake.nix
@@ -1,15 +1,31 @@
 {
-  description = "A very basic flake";
+  description = "Python development shell for work- and research-related stuff.";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        (python312.withPackages (python-pkgs: with python-pkgs; [
+          ase
+          numpy
+          scipy
+          jupyterlab
+          pymatgen
+          matplotlib
+          ovito
+        ]))
+      ];
+      shellHook = ''
+        jupyter-lab
+    '';
+    };
   };
 }
