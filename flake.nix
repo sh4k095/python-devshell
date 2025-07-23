81c24ad26ca4bf1bf8732a08888e4e22812c2e68
sh4k0 <carlofedep@gmail.com>
2025-07-23 16:15:08 +0200

    flake.nix: formatted with alejandra

--- flake.nix
+++ flake.nix
@@ -5,27 +5,30 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in
-    {
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
     devShells.${system}.default = pkgs.mkShell {
       buildInputs = with pkgs; [
-        (python312.withPackages (python-pkgs: with python-pkgs; [
-          ase
-          numpy
-          scipy
-          jupyterlab
-          pymatgen
-          matplotlib
-          ovito
-        ]))
+        (python312.withPackages (python-pkgs:
+          with python-pkgs; [
+            ase
+            numpy
+            scipy
+            jupyterlab
+            pymatgen
+            matplotlib
+            ovito
+          ]))
       ];
       shellHook = ''
         jupyter-lab
-    '';
+      '';
     };
   };
 }
