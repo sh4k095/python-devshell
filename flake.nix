55f512d2a74b822554f172887a74b5a4f3c61804
sh4k0 <carlofedep@gmail.com>
2025-08-01 15:33:33 +0200

    added VASPio to python environment

--- flake.nix
+++ flake.nix
@@ -12,7 +12,22 @@
   } @ inputs: let
     system = "x86_64-linux";
     pkgs = import nixpkgs {inherit system;};
-  in {
+    VASPio = pkgs.python3Packages.buildPythonPackage rec {
+      pname = "vaspio";
+      version = "0.1.0";
+      format = "setuptools";
+      src = pkgs.fetchFromGitHub {
+        owner = "sh4k095";
+        repo = "VASPio";
+        rev = "df0a5432ecc62de15ae8410c14f8b2e96951f3f7";
+        hash = "sha256-Y9WCExaAbm/FWf0IZld9X3cc2fu4cOewKzlhVKlcrbE=";
+      };
+      build-system = with pkgs.python3Packages; [
+        setuptools
+        wheel
+      ];
+    };
+    in {
     devShells.${system}.default = pkgs.mkShell {
       buildInputs = with pkgs; [
         (python312.withPackages (python-pkgs:
@@ -20,15 +35,14 @@
             ase
             numpy
             scipy
-            jupyterlab
             pymatgen
             matplotlib
             ovito
           ]))
-      ];
-      shellHook = ''
-        jupyter-lab ~
-      '';
+      ] ++ [ VASPio ];
+      env = {
+        PSEUDOPATH = "/home/sh4k0/Dropbox/POSTDOC/RESEARCH/POTPAW";
+      };
     };
   };
 }
