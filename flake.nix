ef8d4e12c8e955b93ad34fafe3169217300acfa3
sh4k0 <carlofedep@gmail.com>
2025-09-27 17:15:35 +0200

    flake.nix: added ovito python package

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
     ...
   } @ inputs: let
     system = "x86_64-linux";
-    pkgs = import nixpkgs {inherit system; config.allowUnfree = true;};
+    pkgs = import nixpkgs {inherit system;};
     VASPio = pkgs.python312Packages.buildPythonPackage rec {
       pname = "vaspio";
       version = "0.1.0";
@@ -27,41 +27,80 @@
         pymatgen
       ];
     };
-    tensorpotential = pkgs.python312Packages.buildPythonPackage rec {
-      pname = "tensorpotential";
-      version = "0.5.1";
-      format = "setuptools";
-      src = pkgs.fetchFromGitHub {
-        owner = "ICAMS";
-        repo = "grace-tensorpotential";
-        rev = version;
-        hash = "sha256-nVIHW2aiV79Ul07lqt/juGc8oPYJUeb7TLtqMyOQjGs";
+    ovito = pkgs.python312Packages.buildPythonPackage rec {
+      pname = "ovito";
+      version = "3.14.0";
+      format = "wheel";
+      src = pkgs.fetchurl {
+        url = "https://files.pythonhosted.org/packages/b0/73/87b8f942f02a72d5f39ce1eb842c4290a7ef3e841f48a835f29dbc48b670/ovito-3.14.0-cp312-cp312-manylinux_2_28_x86_64.whl";
+        sha256 = "01cp9j09kangg0v7i5vb9ymfyl62a99yzr2x5rxnbk42070gjjkc";
       };
-      dependencies = let
-        matscipy = pkgs.python312Packages.buildPythonPackage rec {
-          pname = "matscipy";
-          version = "1.1.1";
-          pyproject = true;
-          src = pkgs.fetchFromGitHub {
-            owner = "libAtoms";
-            repo = "matscipy";
-            rev = version;
-            hash = "sha256-mVN+9PTwEMD24KV3Eyp0Jq4vgA1Zs+jThdEbVcfs6pw=";
-          };
-        };
-        in with pkgs.python312Packages; [
-          tensorflow
-          keras
-          scipy
-          matscipy
-          numpy
-          sympy
-          pandas
-          ase
-          pyyaml
-          tqdm
-        ];
+      nativeBuildInputs = [ pkgs.autoPatchelfHook ];
+      autoPatchelfIgnoreMissingDeps = [ "libcuda.so.1" "libnvidia-ml.so.1" ];
+      propagatedBuildInputs = with pkgs.python312Packages; [ numpy traits pyside6  ];
       };
+      #matscipy = pkgs.python312Packages.buildPythonPackage rec {
+      #  pname = "matscipy";
+      #  version = "1.1.1";
+      #  pyproject = true;
+      #
+      #  build-system = with pkgs.python312Packages; [
+      #    meson
+      #    meson-python
+      #    ninja
+      #    cython
+      #    numpy
+      #  ];
+      #
+      #  src = pkgs.fetchFromGitHub {
+      #    owner = "libAtoms";
+      #    repo = "matscipy";
+      #    tag = "v${version}";
+      #    hash = "sha256-mVN+9PTwEMD24KV3Eyp0Jq4vgA1Zs+jThdEbVcfs6pw=";
+      #  };
+      #
+      #  dependencies = with pkgs.python312Packages; [
+      #    numpy #>=1.16.0, <2.0.0
+      #    scipy #>+1.2.3
+      #    ase #>=3.23.0
+      #    packaging
+      #  ];
+      #};
+      #tensorpotential = pkgs.python312Packages.buildPythonPackage rec {
+      #  pname = "tensorpotential";
+      #  version = "0.5.1";
+      #  format = "setuptools";
+      #  src = pkgs.fetchFromGitHub {
+      #    owner = "ICAMS";
+      #    repo = "grace-tensorpotential";
+      #    rev = version;
+      #    hash = "sha256-nVIHW2aiV79Ul07lqt/juGc8oPYJUeb7TLtqMyOQjGs";
+      #  };
+      #  dependencies = let
+      #    matscipy = pkgs.python312Packages.buildPythonPackage rec {
+      #      pname = "matscipy";
+      #      version = "1.1.1";
+      #      pyproject = true;
+      #      src = pkgs.fetchFromGitHub {
+      #        owner = "libAtoms";
+      #        repo = "matscipy";
+      #        rev = version;
+      #        hash = "sha256-mVN+9PTwEMD24KV3Eyp0Jq4vgA1Zs+jThdEbVcfs6pw=";
+      #      };
+      #    };
+      #    in with pkgs.python312Packages; [
+      #      tensorflow
+      #      keras
+      #      scipy
+      #      matscipy
+      #      numpy
+      #      sympy
+      #      pandas
+      #      ase
+      #      pyyaml
+      #      tqdm
+      #    ];
+      #  };
     in {
     devShells.${system}.default = pkgs.mkShell {
       buildInputs = with pkgs; [
@@ -71,9 +110,9 @@
             scipy
             matplotlib
             ovito
+            VASPio
             jupyterlab
-          ]))
-      ] ++ [ VASPio ];
+          ]))];
       env = {
         PSEUDOPATH = "/home/sh4k0/Dropbox/POSTDOC/RESEARCH/POTPAW";
       };
